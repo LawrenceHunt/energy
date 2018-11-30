c94d8b9e30a1a1dc690fc529b95857cf2a1aa487
Lawrence Hunt <lawrence.hunt@hotmail.com>
2018-11-30 18:10:49 +0000

    nothing. just making sure saved

--- energy.js
+++ energy.js
@@ -1,11 +1,10 @@
 const args = process.argv
 const command = process.argv[2]
-const fs = require('fs')
-
 console.log('calling energy.js with args: ', args.slice(2))
 
+const fs = require('fs')
 const data = JSON.parse(fs.readFileSync('./plans.json', 'utf8'));
-console.log('reading data: ', data)
+console.log('loading data: ', data)
 
 console.log('getting keys...', data.forEach(item => console.log(Object.keys(item))))
 
